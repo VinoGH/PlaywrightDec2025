7310377afd37c4d8afef20fdcede1f2ee06bf29f
VinoGH <yoursvinoth@outlook.com>
2025-12-16 23:39:30 +0530

    Week1-Day01

---
+++ Week1-Day01/HW/Day01.js
@@ -0,0 +1,11 @@
+var testerName='vinoth'
+var organizationName='eviden'
+var contactNumber=9791044299
+var isAutomationTester=true
+var hasPlaywrightExperience
+
+console.log("testerName : "+typeof testerName)
+console.log("organizationName : "+typeof organizationName )
+console.log("contactNumber : "+typeof contactNumber)
+console.log("isAutomationTester : "+typeof isAutomationTester)
+console.log("hasPlaywrightExperience is : "+typeof hasPlaywrightExperience)
